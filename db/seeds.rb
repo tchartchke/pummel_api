13861afe83066a2010f8100ff4908de4e0b762bb
tchartchke <chartai.kl@gmail.com>
2021-05-21 22:27:48 -0500

    fixed levelingish. its good enough it hink

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,22 +9,24 @@
 Player.destroy_all
 Level.destroy_all
 
-Player.create(name: 'Alex', score: 30, play_date: Date.today)
-Player.create(name: 'G', score: 20, play_date: Date.today - 1)
-Player.create(name: 'Luha', score: 10, play_date: Date.today - 2)
-Player.create(name: 'Char', score: 14, play_date: Date.today - 3)
+Player.create(name: 'Josh', score: 39, play_date: Date.today - 4)
+Player.create(name: 'Graham', score: 52, play_date: Date.today - 4)
+Player.create(name: 'Rami', score: 71, play_date: Date.today - 3)
+Player.create(name: 'Esme', score: 73, play_date: Date.today - 2)
+Player.create(name: 'Joanna', score: 29, play_date: Date.today - 2)
+Player.create(name: 'Garig', score: 63, play_date: Date.today - 2)
+Player.create(name: 'Jan', score: 26, play_date: Date.today - 2)
+Player.create(name: 'Bobthany', score: 62, play_date: Date.today - 1)
 
 Level.create([
-  {level: 1, passingScore: 5, speedMin: 1000, speedMax: 5000, wait: 1000},
-  {level: 2, passingScore: 10, speedMin: 1000, speedMax: 4000, wait: 800},
-  {level: 3, passingScore: 15, speedMin: 1000, speedMax: 3000, wait: 700},
-  {level: 4, passingScore: 20, speedMin: 800, speedMax: 3000, wait: 700},
-  {level: 5, passingScore: 25, speedMin: 800, speedMax: 2500, wait: 600},
-  {level: 6, passingScore: 30, speedMin: 800, speedMax: 2000, wait: 600},
-  {level: 7, passingScore: 35, speedMin: 700, speedMax: 1500, wait: 500},
-  {level: 8, passingScore: 40, speedMin: 700, speedMax: 1000, wait: 500},
-  {level: 9, passingScore: 45, speedMin: 600, speedMax: 900, wait: 400},
-  {level: 10, passingScore: 40, speedMin: 500, speedMax: 900, wait: 400}
+  {level: 1, passingScore: 5, speedMin: 1500, speedMax: 5000, wait: 500},
+  {level: 2, passingScore: 10, speedMin: 1500, speedMax: 4000, wait: 500},
+  {level: 3, passingScore: 15, speedMin: 1500, speedMax: 3000, wait: 400},
+  {level: 4, passingScore: 20, speedMin: 1200, speedMax: 3000, wait: 400},
+  {level: 5, passingScore: 25, speedMin: 1100, speedMax: 2500, wait: 300},
+  {level: 6, passingScore: 30, speedMin: 1000, speedMax: 2000, wait: 300},
+  {level: 7, passingScore: 35, speedMin: 900, speedMax: 1500, wait: 200},
+  {level: 8, passingScore: 40, speedMin: 900, speedMax: 1000, wait: 200},
+  {level: 9, passingScore: 45, speedMin: 800, speedMax: 900, wait: 100},
+  {level: 10, passingScore: 40, speedMin: 800, speedMax: 900, wait: 100}
 ])
-
-# Note(comment: string, level_id: integer)
